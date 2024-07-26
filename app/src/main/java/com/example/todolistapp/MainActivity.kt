b21a4e53d7dd4770cf8850aad603f4ba41844086
igobhaktiar <igobhaktiar07@gmail.com>
2024-07-26 22:02:06 +0700

    set up ui home

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.todolistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Set the status bar color to white
+        window.statusBarColor = resources.getColor(R.color.white)
+
+        // Set the navigation bar color to white
+        window.navigationBarColor = resources.getColor(R.color.white)
+
+        // Set the navigation bar icon color to black
+        ViewCompat.getWindowInsetsController(binding.root)?.isAppearanceLightNavigationBars = true
+
+        // Set the status bar icon color to black
+        ViewCompat.getWindowInsetsController(binding.root)?.isAppearanceLightStatusBars = true
+
+        // Set the bottom navigation to navigate between fragments
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
     }
 }
